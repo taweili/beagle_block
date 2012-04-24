a1ecd0f913fb7ec14f6f08d3354712c81c073cc5
David Li <taweili@yahoo.com>
2012-04-24 11:55:00 +0800

    add skeleton

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BeagleBlock::Application.config.secret_token = '9f94f0728acd093fafd54db1766b2683fc6ca85c24c22d754b8b01f147d2bb860d6282c2f14a71e5ca2ff7b8f622493441cd28c72973cb310f0c65102a39c342'
